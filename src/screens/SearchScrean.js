861b7297ad40da2b0502bd199c31d1df4b5a1c1f
Ario Tamaddoni <ariozone6@gmail.com>
2019-11-05 21:00:23 -0800

    Extract business logic as useBusinesses hook.#3

--- src/screens/SearchScrean.js
+++ src/screens/SearchScrean.js
@@ -1,27 +1,11 @@
 import React, { useState, useEffect } from "react"
 import { View, Text, StyleSheet } from "react-native"
 import SearchBar from "../components/SearchBar"
-import yelp from "../services/httpServices"
+import useBusinesses from "../hooks/useBusinesses"
 
 const SearchScreen = () => {
   const [query, setQuery] = useState("")
-  const [results, setResults] = useState([])
-  const [errorMessage, setErrorMessage] = useState("")
-
-  useEffect(() => {
-    searchApi()
-  }, [])
-
-  const searchApi = async searchQuery => {
-    try {
-      const response = await yelp.get("/search", {
-        params: { limit: 50, term: searchQuery, location: "irvine" }
-      })
-      setResults(response.data.businesses)
-    } catch (err) {
-      setErrorMessage("Oops! Something went wrong")
-    }
-  }
+  const [searchApi, results, errorMessage] = useBusinesses()
 
   return (
     <View>
