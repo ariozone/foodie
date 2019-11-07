233f7804e9d36db5db21da992843064e6026cd22
Ario Tamaddoni <ariozone6@gmail.com>
2019-11-06 18:04:03 -0800

    Created a new component to render a list of search reasults.#3

---
+++ src/components/BusinessesList.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text, View, StyleSheet } from "react-native"
+
+const BusinessesList = () => {
+  return (
+    <View>
+      <Text>List of Businesses</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({})
+export default BusinessesList
