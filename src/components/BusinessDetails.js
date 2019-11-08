3ed1d3dd00d115e929ab04aee375a80efc944352
Ario Tamaddoni <ariozone6@gmail.com>
2019-11-07 18:40:30 -0800

    Enhanced BusinessDetails component to render businesses images.#4

--- src/components/BusinessDetails.js
+++ src/components/BusinessDetails.js
@@ -4,9 +4,24 @@ import { View, StyleSheet, Text, Image } from "react-native"
 const BusinessDetails = ({ details }) => {
   return (
     <View>
-      <Text>{details.name}</Text>
+      <Image
+        style={styles.imageStyles}
+        source={{ uri: details.image_url }}
+        title={details.name}
+      ></Image>
+      <Text style={styles.nameStyles}>{details.name}</Text>
     </View>
   )
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  imageStyles: {
+    width: 250,
+    height: 120,
+    borderRadius: 5,
+    marginHorizontal: 5
+  },
+  nameStyles: {
+    fontWeight: "bold"
+  }
+})
 export default BusinessDetails
