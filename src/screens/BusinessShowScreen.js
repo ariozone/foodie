f676183651a92eabe23c86aefbbfce45d5fac476
Ario Tamaddoni <ariozone6@gmail.com>
2019-11-08 21:08:32 -0800

    Enhanced BusinessShowScreen to send get request to yelp api and update its state.#5

--- src/screens/BusinessShowScreen.js
+++ src/screens/BusinessShowScreen.js
@@ -1,10 +1,20 @@
 import React from "react"
 import { View, Image, Text, StyleSheet } from "react-native"
+import yelp from "../services/httpServices"
 
 const BusinessShowScreen = ({ navigation }) => {
+  const [business, setBusiness] = React.useState(null)
+  const id = navigation.getParam("id")
+  const getBusiness = async id => {
+    const response = await yelp.get("/" + id)
+    setBusiness(response.data)
+  }
+  React.useEffect(() => {
+    getBusiness(id), []
+  })
   return (
     <View>
-      <Text>BusinessShowScreen: {navigation.getParam("id")} </Text>
+      <Text>BusinessShowScreen: {business && business.name} </Text>
     </View>
   )
 }
